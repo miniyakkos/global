55c65041f48442ca5387e43822505746e234ade9
miniyakkos <ayakokurushima@gmail.com>
2015-10-10 02:31:10 -0700

    raw data in csv and seed file

--- db/migrate/20151010070724_create_members.rb
+++ db/migrate/20151010070724_create_members.rb
@@ -2,6 +2,7 @@ class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
       t.string :username
+      t.string :rank
       t.integer :points
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20151010070724) do
 
   create_table "members", force: :cascade do |t|
     t.string   "username"
+    t.string   "rank"
     t.integer  "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
