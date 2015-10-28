4b9bc274a4c35c6ce69e74c80da331260d4e3255
miniyakkos <ayakokurushima@gmail.com>
2015-10-28 15:36:48 -0700

    Added basic auth, new members page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,9 @@
 class MembersController < ApplicationController
+  http_basic_authenticate_with name:"admin", password: ENV['SECRET'], except: :index
   def index
     @members = Member.paginate(:page => params[:page], :per_page => 50)
+  end
+
+  def new
   end
 end
